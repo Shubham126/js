0cfd231133b9e249628d4c262797d01e80cec917
Subham <chromaticshubham26@gmail.com>
2026-01-24 20:57:40 +0530

    1.js

---
+++ 1.js
@@ -0,0 +1,26 @@
+//1st
+let name1 = "Shubham"
+console.log(name1.length);
+
+//2nd
+let name2 = "Pratibha"
+console.log(name2.slice(1, 4));
+
+//3th
+let name3 = "Cheeku"
+console.log(name3.slice(2));
+
+//4th
+let name4 = "Subham Kumar"
+console.log(name4.replace("Kumar", "Yadav"));
+
+//5th
+
+let fruit1 = "Apple";
+let fruit2 = "Mango";
+let fruits = fruit1.concat(" ", fruit2);    
+console.log(fruits);
+
+//6th 
+let text1 = "    Hello World!  I  am  revising  Javascript    ";
+console.log(text1.trim());
