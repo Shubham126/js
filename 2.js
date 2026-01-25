75ccbde19f059ae45cfc64b04b27eda941532687
Subham <chromaticshubham26@gmail.com>
2026-01-25 23:40:23 +0530

    array methods

--- 2.js
+++ 2.js
@@ -1,3 +1,26 @@
 let arr = [1, 2, 3];
+
 console.log(arr);
+
+//1
 console.log(arr.toString());
+
+//2
+console.log(arr.join(" - "));
+
+//3
+arr.pop();
+console.log(arr);
+
+//4 
+arr.push(4);
+console.log(arr);
+
+//5 
+arr.shift();
+console.log(arr);
+
+//6
+arr.unshift(0);
+console.log(arr);
+
