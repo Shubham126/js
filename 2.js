808629f7f21ab0a7136fa65abd27c1f18c760495
Subham <chromaticshubham26@gmail.com>
2026-01-27 07:05:30 +0530

    array functions

--- 2.js
+++ 2.js
@@ -1,26 +1,10 @@
+//basics of array
+
 let arr = [1, 2, 3];
-
+console.log(arr);
+console.log(arr.length);
+console.log(arr[1]);
+arr[1] = 8;
 console.log(arr);
 
-//1
-console.log(arr.toString());
-
-//2
-console.log(arr.join(" - "));
-
-//3
-arr.pop();
-console.log(arr);
-
-//4 
-arr.push(4);
-console.log(arr);
-
-//5 
-arr.shift();
-console.log(arr);
-
-//6
-arr.unshift(0);
-console.log(arr);
-
+console.log(typeof(arr));
