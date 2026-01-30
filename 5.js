899b5b4ee7c2fca1c51f0166806a1f2c0bd1afa1
Subham <chromaticshubham26@gmail.com>
2026-01-30 15:14:10 +0530

    Iteration

--- 5.js
+++ 5.js
@@ -1,5 +1,19 @@
-let sentence = ["Hello", "world", "this", "is", "JavaScript"];
+let numbers = [1, 2, 3, 4, 5]
 
-for(let i = 0; i<sentence.length; i++){
-    console.log(sentence[i]);
+for(let i = 0; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+numbers.forEach((value, index)=>{
+    console.log("Value at Position "+index+" is "+value);
+})
+
+let colours =  ["Red", "green", "blue"];
+for( let colour of colours){
+    console.log(colour);
+}
+
+let animals =  ["Dog", "Cat", "Elephant"];
+for(let index in animals){
+    console.log(animals[index]);
 }
