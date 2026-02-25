703b6aced42b975beb1e7d66612000ce73f71eef
Subham <chromaticshubham26@gmail.com>
2026-02-25 23:42:16 +0530

    async await

--- 17/syncAndAsync.js
+++ 17/syncAndAsync.js
@@ -6,4 +6,14 @@ console.log("step 4");
 setTimeout(()=> {
     console.log("step 5");
 }, 2000);
-console.log("step 6")
+console.log("step 6")   
+
+async function test() {
+    console.log("A");
+    await Promise.resolve("B");
+    console.log("C");
+}
+
+console.log("Start");
+test();
+console.log("End");
