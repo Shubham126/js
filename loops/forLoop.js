96edcd70d7a36e54714b51395112b77b4460767a
Subham <chromaticshubham26@gmail.com>
2026-03-19 20:52:34 +0530

    for loop

---
+++ loops/forLoop.js
@@ -0,0 +1,21 @@
+const boys = [
+    {
+        name: "abc",
+        age: 22,
+        fname: "def"    
+    },
+    {
+        name: "abc",
+        age: 22,
+        fname: "def"    
+    },
+    {
+        name: "abc",
+        age: 22,
+        fname: "def"    
+    }
+];
+
+for( let i = 0 ; i < boys.length; i++){
+    console.log(boys[i]);
+}
