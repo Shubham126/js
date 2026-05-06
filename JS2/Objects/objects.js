d1312ee7e7b490ae7851e1daae53eba8fb5eeb94
Subham <chromaticshubham26@gmail.com>
2026-05-07 02:32:33 +0530

    Objects 2

--- JS2/Objects/objects.js
+++ JS2/Objects/objects.js
@@ -4,7 +4,24 @@
 
 // object literal
 
+const mySym = Symbol("key1")
+
 const JsUser = {
-    name : "Shubham"
+    name : "Shubham",
+    "full name": "Shubham Kumar",
+    location: "Delhi",
+    [mySym]: "mykey1",// this is a way to declare a symbol key in an object
+    age: 24,
+    email: "chromaticshubham26@gmail.com",
+    isLoggesIn: false,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
 } 
 
+// access objects
+console.log(JsUser.name);
+console.log(JsUser["name"]);
+// full name can't be accessed by dot notation because of space in between
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]); // This is how we can access the symbol key in an object
+
+Object.freeze(JsUser) // this will make the object immutable, we can't change the values of the object after freezing it
---
+++ JS2/Objects/objects2.js
@@ -0,0 +1,16 @@
+const User = {
+    name: "John",
+    age: 30,
+    email: "chromaticshubhan@26",
+    location: "Delhi",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+}
+
+const greetings = () => {
+    console.log("hello world");
+}
+
+greetings();
+
+console.log(greetings); // this will print the function definition of greetings
